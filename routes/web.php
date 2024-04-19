045dbdba9c648cd5c104b12571a7cb5d90e98426
AliAmin565 <techali565@gmail.com>
2024-04-19 17:05:14 +0200

    Add DashBoardURL

--- routes/web.php
+++ routes/web.php
@@ -22,16 +22,15 @@ Route::get("/packages", function () {
     return view('front.package');
 });
 
+// Dashboard Page
 
+Route::get("/dashboard-admin", function () {
+    return view('dashboard.index');
+});
 
-
-
-
-
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
